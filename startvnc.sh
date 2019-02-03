eed3d5cb7c91b8ad78f9cdf22e261f1ffa43bf27
masewo <masewo@gmx.net>
2019-02-03 14:34:00 +0100

    hopefully first working start & stop

--- startvnc.sh
+++ startvnc.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+
 rm /usr/bin/Xtightvnc
 ln -s /usr/bin/Xtightvnc.original /usr/bin/Xtightvnc
+
 if [ ! -f /usr/bin/vncserver.modified ]
 then
   cp /usr/bin/vncserver /usr/bin/vncserver.modified
 fi
 rm /usr/bin/vncserver
 ln -s /usr/bin/vncserver.original /usr/bin/vncserver
+
+sed -i '/#SRVTYPE=wrapped_tight/,/#SRVTYPE=wrapped_tight/{s/#SRVTYPE=wrapped_tight/SRVTYPE=wrapped_tight/}' /etc/init.d/vnc.sh
+sed -i '/SRVTYPE=wrapped_tiger/,/SRVTYPE=wrapped_tiger/{s/^SRVTYPE=wrapped_tiger/#SRVTYPE=wrapped_tiger/}' /etc/init.d/vnc.sh
+sed -i '/wrapped_tight)/,/sudo/{s/-localhost //}' /etc/init.d/vnc.sh
+
+GEOMETRY=${1:-2560x1800}
+$DPI=${2:-308}
+
+/etc/init.d/vnc.sh $GEOMETRY $DPI
--- stopvnc.sh
+++ stopvnc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 rm /usr/bin/Xtightvnc
 ln -s /opt/linux_on_dex/usr/bin/Xtightvnc /usr/bin/Xtightvnc
 
@@ -9,6 +10,5 @@ fi
 rm /usr/bin/vncserver
 ln -s /usr/bin/vncserver.modified /usr/bin/vncserver
 
-sed '0,/SRVTYPE=wrapped_tight/{s/SRVTYPE=wrapped_tight/#SRVTYPE=wrapped_tight}' /etc/init.d/vnc.sh
-sed '0,/#SRVTYPE=wrapped_tiger/{s/#SRVTYPE=wrapped_tiger/SRVTYPE=wrapped_tiger}' /etc/init.d/vnc.sh
-#sed '' /etc/init.d/vnc.sh 
+sed -i '/SRVTYPE=wrapped_tight/,/SRVTYPE=wrapped_tight/{s/^SRVTYPE=wrapped_tight/#SRVTYPE=wrapped_tight/}' /etc/init.d/vnc.sh
+sed -i '/#SRVTYPE=wrapped_tiger/,/#SRVTYPE=wrapped_tiger/{s/#SRVTYPE=wrapped_tiger/SRVTYPE=wrapped_tiger/}' /etc/init.d/vnc.sh
